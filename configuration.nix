deb0fdfeecf2e836a5aa60bf82516d3e593960c5
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2026-03-06 09:51:11 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,6 @@
 { config, pkgs, ... }:
 
-let
-  hardwareConfig =
-    if builtins.pathExists /mnt/etc/nixos/hardware-configuration.nix then
-      /mnt/etc/nixos/hardware-configuration.nix
-    else
-      /etc/nixos/hardware-configuration.nix;
-in
 {
-  imports = [
-    hardwareConfig
-  ];
-
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   networking.hostName = "nixos";
@@ -34,9 +23,7 @@ in
     wallpaper = "/home/luvier/wallpaper.jpg";
   };
 
-  home-manager.users.luvier = {
-    home.stateVersion = "24.11";
-  };
+  home-manager.users.luvier.home.stateVersion = "24.11";
 
   system.stateVersion = "24.11";
 }
