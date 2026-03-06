1c862dc896ab8c5bb97dc4088d3e9a8c9c5df066
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2026-03-06 10:19:07 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -5,7 +5,12 @@
     ./hardware-configuration.nix
   ];
 
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
 
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
