1bcab9887d5bb24d79f358234d6c22b3ed050b36
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2026-03-10 11:12:38 -0300

    Update jupyter.nix

--- shells/jupyter.nix
+++ shells/jupyter.nix
@@ -1,38 +1,40 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-    onnx2c = pkgs.callPackage ./pkgs/onnx2c.nix {};
+        onnx2c = pkgs.callPackage ./pkgs/onnx2c.nix {};
 
-    pythonEnv = pkgs.python3.withPackages (p: with p; [
-      ipykernel
-      jupyterlab
-      torch
-      torchvision
-      tqdm
-      matplotlib
-      torchinfo
-      onnxscript
-    ]);
+        pythonEnv = pkgs.python3.withPackages (p: with p; [
+          ipykernel
+          jupyterlab
+          torch
+          torchvision
+          tqdm
+          matplotlib
+          torchinfo
+          onnxscript
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pythonEnv
+            onnx2c
+            gcc
+            gnumake
+            cmake
+          ];
 
-  in {
-
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [
-        pythonEnv
-        onnx2c
-        gcc
-        gnumake
-        cmake
-      ];
-
-      shellHook = "jupyter lab";
-    };
-  };
+          shellHook = ''
+            jupyter lab
+          '';
+        };
+      }
+    );
 }
