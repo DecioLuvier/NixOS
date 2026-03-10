9df430ba9d41039791d0e329c3b1db33b94d9264
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2026-03-10 10:21:32 -0300

    Update luvier.nix

--- hosts/laptop/profile/luvier.nix
+++ hosts/laptop/profile/luvier.nix
@@ -2,13 +2,13 @@
 
 {
   imports = [
-    ../../modules/hyprland.nix
-    ../../modules/swaybg.nix
-    ../../modules/mako.nix
-    ../../modules/waybar.nix
-    ../../modules/wlogout.nix
-    ../../modules/wofi.nix
-    ../../modules/vscode.nix
+    ../../../modules/hyprland.nix
+    ../../../modules/swaybg.nix
+    ../../../modules/mako.nix
+    ../../../modules/waybar.nix
+    ../../../modules/wlogout.nix
+    ../../../modules/wofi.nix
+    ../../../modules/vscode.nix
   ];
 
   services.greetd = {
