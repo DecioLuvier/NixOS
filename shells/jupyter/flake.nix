a1742117475f7e0d11db906c9ed1669f640ecac2
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2026-03-15 10:55:26 -0300

    Update flake.nix

--- shells/jupyter/flake.nix
+++ shells/jupyter/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Jupyter dev environment package with Python and VSCodium";
+  description = "Jupyter dev environment with Python Full/Mini and VSCodium";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -10,39 +10,25 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        
         kernels = import ./kernels.nix { inherit pkgs; };
         settings = import ./settings.nix { inherit pkgs; };
         extensions = import ./extensions.nix { inherit pkgs; };
-
-        codium-with-extensions = pkgs.vscode-with-extensions.override {
+ 
+        codium = pkgs.vscode-with-extensions.override {
           vscode = pkgs.vscodium;
           vscodeExtensions = extensions;
         };
-
-        jupyter-env = pkgs.writeShellScriptBin "jupyter-env" ''
-          # Estas pastas são criadas onde você RODAR o comando, não no build
+      in {
+        devShells.default = pkgs.mkShell {
+        packages = [ codium kernels.pythonFull ];
+        shellHook = ''
           mkdir -p "$PWD/.kernels"
           mkdir -p "$PWD/.vscode/User"
-          
-          # Processa o settings.json
           sed "s|__PWD__|$PWD|g" ${settings} > "$PWD/.vscode/User/settings.json"
-          
-          # Garante que o Python do kernel esteja no PATH ao abrir o Codium
-          export PATH="${kernels.pythonFull}/bin:$PATH"
-          
-          # Executa o Codium isolado
-          exec ${codium-with-extensions}/bin/codium --user-data-dir="$PWD/.vscode" "$PWD"
+          codium --user-data-dir="$PWD/.vscode" "$PWD"
         '';
-      in {
-        packages.default = jupyter-env;
-
-        devShells.default = pkgs.mkShell {
-          packages = [ jupyter-env ];
-          shellHook = ''
-            echo "Ambiente carregado. Digite 'jupyter-env' para iniciar."
-          '';
         };
+
       }
     );
 }
