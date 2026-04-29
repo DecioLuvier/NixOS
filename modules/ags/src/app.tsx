5625c973443698db8ab20cabd2f6387faf89486a
decioluvier <decioluvieriii@gmail.com>
2026-04-29 09:03:05 -0300

    .

--- modules/ags/src/app.tsx
+++ modules/ags/src/app.tsx
@@ -1,10 +1,8 @@
 import app from "ags/gtk4/app"
 
-import style from "./style.scss"
-import Bar from "./waybar/waybar"
+import Bar from "./widgets/waybar/waybar"
 
 app.start({
-  css: style,
   main() {
     app.get_monitors().map((monitor) =>
       Bar({ gdkmonitor: monitor })
