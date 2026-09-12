3c495e5217e6231d824c26809ca77b75868b46e9
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2026-09-12 11:00:01 -0300

    .

--- packages/Bars/default.nix
+++ packages/Bars/default.nix
@@ -5,12 +5,25 @@ pkgs.stdenv.mkDerivation {
   version = "0.1.0";
   src = ./.;
 
-  nativeBuildInputs = [ pkgs.cmake pkgs.pkg-config pkgs.wrapGAppsHook4 pkgs.makeWrapper ];
+  nativeBuildInputs = [ pkgs.pkg-config pkgs.wrapGAppsHook4 pkgs.makeWrapper ];
   buildInputs = [ pkgs.gtk4 pkgs.gtk4-layer-shell ];
 
-  postInstall = ''
-    mkdir -p $out/share/bars
+  dontConfigure = true;
+
+  buildPhase = ''
+    runHook preBuild
+    g++ -std=c++20 -o bars \
+      src/main.cpp src/bar_window.cpp \
+      $(pkg-config --cflags --libs gtk4 gtk-layer-shell-0)
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin $out/share/bars
+    cp bars $out/bin/bars
     cp ${./style.css} $out/share/bars/style.css
+    runHook postInstall
   '';
 
   # runs after wrapGAppsHook4's own wrapping, appends our env var
